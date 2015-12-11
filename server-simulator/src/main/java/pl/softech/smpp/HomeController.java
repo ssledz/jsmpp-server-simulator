6622afd4902eec1d8f4010ee3e3a0db4102be3a2
ssledz <slawomir.sledz@gmail.com>
2015-12-11 01:22:08 +0100

    bzz

--- server-simulator/src/main/java/pl/softech/smpp/HomeController.java
+++ server-simulator/src/main/java/pl/softech/smpp/HomeController.java
@@ -1,16 +1,20 @@
 package pl.softech.smpp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
+    public String home(Model model) {
+        model.addAttribute("time", new Date());
+        model.addAttribute("message", "Hello World");
+        return "home";
     }
 
 }
---
+++ server-simulator/src/main/resources/templates/home.ftl
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <title>Smpp Server Simulator</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+</head>
+<body>
+    Date: ${time?date}
+    <br>
+    Time: ${time?time}
+    <br>
+    Message: ${message}
+</body>
+</html>
